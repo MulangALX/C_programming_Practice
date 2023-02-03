dc72250a8fcadc6d526e815df782a6e0bf62b5d2
Martin Oduor Mulang <mulangmartin2017@gmail.com>
2023-02-03 10:22:44 +0300

    MOM ahs changed something

--- python_with_git.py
+++ python_with_git.py
@@ -1,5 +1,7 @@
 def area_of_circle(r):
     A = 22/7 * r**2
     print("The area of a circle of raduis {}, is {}".format(r, A))
-    
-    return A     
+
+name = input("Enter Your name")
+my_radius = input("ENter the radius of the circle whose area you want to calculate")
+print(name, "Below is the result: /n", area_of_circle(my_radius))
