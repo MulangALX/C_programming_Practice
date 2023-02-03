2eee2bc929e9c02d79f3a8b3d567d5e3f8f10955
Martin Oduor Mulang <mulangmartin2017@gmail.com>
2023-02-03 19:57:17 +0300

    I did it, I made it happen

--- CC.c
+++ CC.c
@@ -1,3 +1,17 @@
-int y = 3
-int x = 4
-printf("x + y = %d", x+y)
+#include <stdio.h>
+void main(){
+	int y = 3;
+	int x = 4;
+	printf("x + y = %d", x+y);
+	//this is the fisrt C code it wil print hese lines in  a straigt line
+	//I hope it pays off
+	//And I know it will in the ens
+	};
+/*
+ 
+   the is int it beautiful
+ to know these things is is genius
+I will endeavur to learn at all costs
+This I have done. I MULANG' HAVE SPKEN
+
+*/
