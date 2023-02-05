b6a1a8614a0fcc1365d84e66f1c4b2851114ac2e
Martin Oduor Mulang <mulangmartin2017@gmail.com>
2023-02-05 11:44:58 +0300

    Fun indeed, I have learnt TYPE CONVERSSIONS

--- CC.c
+++ CC.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
+#include </>
 void main(){
 	int y = 3;
 	int x = 4;
-	printf("x + y = %d", x+y);
+	printf("x + y = %d\n", x+y);
 	//this is the fisrt C code it wil print hese lines in  a straigt line
 	//I hope it pays off
 	//And I know it will in the ens
-	};
+
 /*
  
    the is int it beautiful
@@ -14,4 +15,20 @@ void main(){
 I will endeavur to learn at all costs
 This I have done. I MULANG' HAVE SPKEN
 
-*/
+
+the e cintinue wtih the thingy todat the 5th of february 2022*/
+
+
+	printf("%s is the best %s ever since %d\n\n", "Caroline", "Girlfrienid", 2019);
+	printf("%f Pi to NO decimal places is %f \n", 1.,  3.14675436);
+        printf("Pi to TWO decimal places is %.2f\n", 3.14675436);
+
+
+        printf("Pi to no decimal places is %f\n\n", 3.14675436);
+
+	int age;
+	age = 29;
+	printf("%s is %d years old while %s is %d years old \n may they live as long as %s \n","Martin Mulang'",29, "Isaiah Onando", 35, "Ogago Nyalwanda");
+
+	};
+
